f06dfbade5e1ba3ecda7ede5cfc44e74d41e22ac
Robin Hurtado <robin12898@hotmail.com>
2025-06-11 21:28:58 -0700

    first react update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
 import './App.css';
+import Nav from './Nav';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <Nav/>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </>
   );
 }
 
