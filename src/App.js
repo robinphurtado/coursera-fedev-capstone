d4886f6e6507160ba3bd1d52c07476e961c6fffb
Robin Hurtado <robin12898@hotmail.com>
2025-06-11 23:45:27 -0700

    styling components and page framework

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 
 import './App.css';
-import Nav from './Nav';
-import Header from './Header';
-import Main from './Main';
-import Footer from './Footer';
+import Nav from './components/Nav';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
